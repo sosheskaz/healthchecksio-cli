299ba4625d148b88877c2a1c438d1aaa152b4c8b
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2025-11-30 14:10:19 -0600

    ci: pat github token

--- cmd/root.go
+++ cmd/root.go
@@ -27,45 +27,10 @@ func mustWrite(w io.Writer, msg string) {
 	}
 }
 
-func setGlobalCommandFlags(cmd *cobra.Command) {
-	input, err := cmd.Flags().GetString("input")
-	if err == nil && input != "" {
-		reader, err := os.Open(input)
-		if err != nil {
-			panic(fmt.Sprintf("failed to open input file %q: %+v", input, err))
-		}
-		cmd.SetIn(reader)
-	}
-	output, err := cmd.Flags().GetString("output")
-	if err == nil && output != "" {
-		writer, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-		if err != nil {
-			panic(fmt.Sprintf("failed to open output file %q: %+v", output, err))
-		}
-		cmd.SetOut(writer)
-	}
-
-	oldPostRun := cmd.PostRun
-	cmd.PostRun = func(cmd *cobra.Command, args []string) {
-		if oldPostRun != nil {
-			oldPostRun(cmd, args)
-		}
-		if input != "" {
-			_ = cmd.InOrStdin().(*os.File).Close()
-		}
-		if output != "" {
-			_ = cmd.OutOrStdout().(*os.File).Close()
-		}
-	}
-}
-
 func rootCommand() *cobra.Command {
 	c := &cobra.Command{
 		Use:   "healthchecksio-cli <check_id> [<signal>]",
 		Short: "Call healthchecks.io checks from the command line",
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			setGlobalCommandFlags(cmd)
-		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var (
 				checkId string
