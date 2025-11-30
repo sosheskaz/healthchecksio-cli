c98d39c30c0095144c01a0cbb2da8b53a9162049
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2025-11-30 12:47:45 -0600

    fix: command handles unexpected status codes better

--- cmd/root.go
+++ cmd/root.go
@@ -17,66 +17,112 @@ const (
 `
 )
 
+var (
+	topCommands = make([]func() *cobra.Command, 0)
+)
+
 func mustWrite(w io.Writer, msg string) {
 	if _, err := w.Write([]byte(msg)); err != nil {
 		panic(err)
 	}
 }
 
-var rootCmd = &cobra.Command{
-	Use:   "healthchecksio-cli <check_id> [<signal>]",
-	Short: "Call healthchecks.io checks from the command line",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		var (
-			checkId string
-			signal  string
-		)
-
-		if len(args) == 0 {
-			mustWrite(cmd.ErrOrStderr(), rootCmdUsage)
-			return errors.New("please provide a check id")
-		}
-		if len(args) > 2 {
-			mustWrite(cmd.ErrOrStderr(), rootCmdUsage)
-			return fmt.Errorf("extraneous arguments found: %v", args[2:])
-		}
-		checkId = args[0]
-		if len(args) > 1 {
-			signal = args[1]
-		}
-
-		mustWrite(cmd.ErrOrStderr(), "calling check "+checkId)
-		if signal != "" {
-			mustWrite(cmd.ErrOrStderr(), " with signal "+signal)
-		}
-		mustWrite(cmd.ErrOrStderr(), "\n")
-
-		url := "https://hc-ping.com/" + checkId
-		if signal != "" {
-			url += "/" + signal
-		}
-
-		resp, err := http.Get(url)
+func setGlobalCommandFlags(cmd *cobra.Command) {
+	input, err := cmd.Flags().GetString("input")
+	if err == nil && input != "" {
+		reader, err := os.Open(input)
 		if err != nil {
-			return err
+			panic(fmt.Sprintf("failed to open input file %q: %+v", input, err))
 		}
-		defer func() { _ = resp.Body.Close() }()
-		if resp.StatusCode != 200 {
-			return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		cmd.SetIn(reader)
+	}
+	output, err := cmd.Flags().GetString("output")
+	if err == nil && output != "" {
+		writer, err := os.OpenFile(output, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			panic(fmt.Sprintf("failed to open output file %q: %+v", output, err))
 		}
+		cmd.SetOut(writer)
+	}
 
-		return nil
-	},
-}
-
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
+	oldPostRun := cmd.PostRun
+	cmd.PostRun = func(cmd *cobra.Command, args []string) {
+		if oldPostRun != nil {
+			oldPostRun(cmd, args)
+		}
+		if input != "" {
+			_ = cmd.InOrStdin().(*os.File).Close()
+		}
+		if output != "" {
+			_ = cmd.OutOrStdout().(*os.File).Close()
+		}
 	}
 }
 
-func init() {
-	rootCmd.InitDefaultCompletionCmd()
-	rootCmd.Args = cobra.RangeArgs(1, 2)
+func rootCommand() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "healthchecksio-cli <check_id> [<signal>]",
+		Short: "Call healthchecks.io checks from the command line",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			setGlobalCommandFlags(cmd)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var (
+				checkId string
+				signal  string
+			)
+
+			if len(args) == 0 {
+				mustWrite(cmd.ErrOrStderr(), rootCmdUsage)
+				return errors.New("please provide a check id")
+			}
+			if len(args) > 2 {
+				mustWrite(cmd.ErrOrStderr(), rootCmdUsage)
+				return fmt.Errorf("extraneous arguments found: %v", args[2:])
+			}
+			checkId = args[0]
+			if len(args) > 1 {
+				signal = args[1]
+			}
+
+			mustWrite(cmd.ErrOrStderr(), "calling check "+checkId)
+			if signal != "" {
+				mustWrite(cmd.ErrOrStderr(), " with signal "+signal)
+			}
+			mustWrite(cmd.ErrOrStderr(), "\n")
+
+			url := "https://hc-ping.com/" + checkId
+			if signal != "" {
+				url += "/" + signal
+			}
+
+			resp, err := http.Get(url)
+			if err != nil {
+				return err
+			}
+			defer func() { _ = resp.Body.Close() }()
+			if resp.StatusCode != 200 {
+				return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+			}
+
+			return nil
+		},
+	}
+
+	for _, topCmd := range topCommands {
+		c.AddCommand(topCmd())
+	}
+
+	return c
+}
+
+func Command() *cobra.Command {
+	return rootCommand()
+}
+
+func Execute() {
+	err := Command().Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
---
+++ gen.go
@@ -0,0 +1,36 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/sosheskaz/healthchecksio-cli/cmd"
+)
+
+func main() {
+	outDir := path.Join("completions")
+
+	if err := os.RemoveAll(outDir); err != nil {
+		panic(fmt.Sprintf("failed to remove output directory %q: %+v", outDir, err))
+	}
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		panic(fmt.Sprintf("failed to create output directory %q: %+v", outDir, err))
+	}
+
+	cmd := cmd.Command()
+	errs := []error{
+		cmd.GenBashCompletionFileV2(path.Join(outDir, "bash_completion.sh"), true),
+		cmd.GenZshCompletionFile(path.Join(outDir, "zsh_completion.sh")),
+		cmd.GenFishCompletionFile(path.Join(outDir, "fish_completion.fish"), true),
+		cmd.GenPowerShellCompletionFileWithDesc(path.Join(outDir, "powershell_completion.ps1")),
+	}
+	for _, err := range errs {
+		if err != nil {
+			panic(fmt.Sprintf("failed to generate completion files: %+v", err))
+		}
+	}
+}
