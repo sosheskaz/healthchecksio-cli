424ece15917155be86368dcdf179385c0af1daf9
Eric Miller <1588827+sosheskaz@users.noreply.github.com>
2026-05-24 22:05:11 -0500

    ci: add mise-backed lint and release validation

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
-//go:generate go run gen.go
+// Package main is the entrypoint for the healthchecks.io CLI
 
+//go:generate go run gen.go
 package main
 
 import "github.com/sosheskaz/healthchecksio-cli/cmd"
